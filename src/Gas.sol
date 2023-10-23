462c7d45b7a3b9e0559b49d9a37d503235570129
András Szücs <andrew0szucs@gmail.com>
2023-10-23 14:54:48 +0000

    Change totalSupply to private

--- src/Gas.sol
+++ src/Gas.sol
@@ -6,7 +6,7 @@ import "./Ownable.sol";
 uint8 constant ADMINS_LENGTH = 5;
 
 contract GasContract is Ownable {
-    uint256 public totalSupply = 0; // cannot be updated
+    uint256 private totalSupply = 0; // cannot be updated
     uint256 public paymentCounter = 0;
     mapping(address => uint256) public balances;
     uint256 public tradePercent = 12;
